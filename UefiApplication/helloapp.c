2ac02ac462f3c714431647592403b840812982e0
fuss132 <admin@fussbot.io>
2018-05-22 18:08:51 +0200

    Add setupGUI function

--- UefiApplication/helloapp.c
+++ UefiApplication/helloapp.c
@@ -1,21 +1,15 @@
-//
 // Basic UEFI Libraries
-//
 #include <Uefi.h>
 #include <Library/UefiLib.h>
 #include <Library/DebugLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/BaseMemoryLib.h>
 
-//
 // Boot and Runtime Services
-//
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
 
-//
 // Shell Library
-//
 #include <Library/ShellLib.h>
 
 // We run on any UEFI Specification
@@ -23,6 +17,11 @@ extern CONST UINT32 _gUefiDriverRevision = 0;
 
 // Package name
 CHAR8 *gEfiCallerBaseName = "TestApp";
+
+//
+// Function prototypes
+//
+EFI_STATUS setupGUI(EFI_SYSTEM_TABLE*);
 
 EFI_STATUS
 EFIAPI
@@ -48,12 +47,35 @@ UefiMain (IN EFI_HANDLE image_handle, IN EFI_SYSTEM_TABLE* system_table)
 		return efiStatus;
 	}
 
+	// Setup GUI
+	efiStatus = setupGUI(system_table);
+	if (EFI_ERROR(efiStatus))
+	{
+		Print(L"Failed to initialize GUI: %lx\n", efiStatus);
+		return efiStatus;
+	}
+
+	
+
+    // Print Hello World
+	Print(L"Hello World!");
+
+	return EFI_SUCCESS;
+}
+
+//
+// Function to setup screen resolution by determining
+// the best available mode & activating it
+//
+EFI_STATUS setupGUI(EFI_SYSTEM_TABLE* system_table) {
+	// Disable cursor
+	system_table->ConOut->EnableCursor(system_table->ConOut, FALSE);
+
+	// Clear input buffer
+	system_table->ConIn->Reset(system_table->ConIn, FALSE);
+
 	// Clear screen
 	system_table->ConOut->ClearScreen(system_table->ConOut);
 
-    // Print Hello World
-    Print(L"Hello World!");
-
-    return efiStatus;
+	return EFI_SUCCESS;
 }
-
